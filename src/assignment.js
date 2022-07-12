cbe8082b266aef23b7af721ba156aa2198cd883b
SundayOlubode <63305676+SundayOlubode@users.noreply.github.com>
2022-07-12 13:17:24 +0000

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sumOfAll = 0;
+    arrayOfNumbers.forEach(elem => sumOfAll += elem)
+    return sumOfAll;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +30,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenFromArr = arrayOfNumbers.filter(elem => elem % 2 === 0)
+    return evenFromArr.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
